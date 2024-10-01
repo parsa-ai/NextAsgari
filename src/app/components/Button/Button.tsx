c43e130b7746dad6ab4a19101db5d5d2b78780e5
S.Mohammad.Amin.Jafarnezhad <mohammadaminjafarnezhad6@gmail.com>
2024-10-01 18:54:58 +0330

    add important color

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -5,14 +5,14 @@ export default function Button({ varient, text, icon }: buttonProps) {
   switch (varient) {
     case "outline":
       return (
-        <a href="#" className="button-outline">
+        <a href="#" className="button outline">
           {icon ? <img src={icon} className="ml-2" /> : null}
           {text}
         </a>
       );
     case "contained":
       return (
-        <a href="#" className="bg-main-950 py-2.5 px-4 rounded-3xl text-white flex flex-row-reverse">
+        <a href="#" className="button contained">
           {icon ? <img src={icon} className="ml-2" /> : null}
           {text}
         </a>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,34 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        main: "#6262",
+        black: "#000",
+        white: "#fff",
+        information: {
+          50: "#EFF6FF",
+          100: "#DBEAFE",
+          200: "#BFDBFE",
+          300: "#93C5FD",
+          400: "#60A5FA",
+          500: "#3B82F6",
+          600: "#2563EB",
+          700: "#1D4ED8",
+          800: "#1E40AF",
+          900: "#1E3A8A",
+          950: "#172E54",
+        },
+        light: {
+          50: "#F5F5F6",
+          100: "#E5E6E8",
+          200: "#CDCFD4",
+          300: "#ABAFB5",
+          400: "#81858F",
+          500: "#6D727D",
+          600: "#575A63",
+          700: "#4A4D54",
+          800: "#424448",
+          900: "#3A3B3F",
+          950: "#242528",
+        },
       },
       fontFamily: {
         anjoman: ["var(--Anjoman)"],
